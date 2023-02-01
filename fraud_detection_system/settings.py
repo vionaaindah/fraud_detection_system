689fb226e14d6936679dde2069671876fb8b8a65
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2023-02-01 08:09:50 +0700

    Update settings.py

--- fraud_detection_system/settings.py
+++ fraud_detection_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'fraud_detection_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
