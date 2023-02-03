7a3f795f2f44c15ed0b2b7e143a3640246b78c7e
Siti Viona Indah Swari <69779014+vionaaindah@users.noreply.github.com>
2023-02-03 10:00:57 +0700

    Update views.py

--- api_detection/views.py
+++ api_detection/views.py
@@ -5,6 +5,7 @@ from api_detection.models import digi_login_activity
 from django.http import JsonResponse
 import json
 from django.shortcuts import render
+from datetime import datetime
 
 class FraudDetection(APIView):
     def get(self, request):
@@ -49,6 +50,50 @@ class fraudDetection(APIView):
 
         for i, row in data.iterrows():
             data_pred = [[int(row.customer_id), row.activity_date,  row.device_name,  row.device_type,  row.device_os, float(row.latitude), float(row.longitude)]]
+            data_pred = pd.DataFrame(data_pred, columns =['customer_id', 'activity_date', 'device_name', 'device_type', 'device_os', 'latitude', 'longitude'])
+            data_pred['activity_date'] = pd.to_datetime(data_pred['activity_date'], format='%Y-%m-%d %H:%M:%S.%f').apply(lambda x: x.timestamp())
+            data_pred['device_name'] = data_pred['device_name'].apply(lambda x: sum(ord(c) for c in x))
+            data_pred['device_type'] = data_pred['device_type'].apply(lambda x: sum(ord(c) for c in x))
+            data_pred['device_os'] = data_pred['device_os'].apply(lambda x: sum(ord(c) for c in x))
+            data_pred_scaled = scaler.transform(data_pred)
+
+            result = model.predict(data_pred_scaled)
+
+            data.at[i,'result'] = result
+            if (result==1):
+                data['keterangan'].at[i] = pd.Series({1: 'Aktivitas login dilakukan di lokasi yang jauh berbeda dari aktivitas login lain'})
+            elif(result==2):
+                data['keterangan'].at[i] = pd.Series({1: 'Aktivitas login dilakukan pada device yang berbeda'})
+            elif(result==3):
+                data['keterangan'].at[i] = pd.Series({1: 'Aktivitas login dilakukan pada device yang berbeda', 2: 'Aktivitas login dilakukan di lokasi yang jauh berbeda dari aktivitas login lain'})
+       
+        data_result = data.to_json(orient='records')
+        response = json.loads(data_result)
+        return JsonResponse(response, safe=False)
+    
+    def post(self, request):
+        data_request = request.data
+        model = sett.model
+        scaler = sett.scale
+
+        customer_id = int(data_request['customer_id'])
+        app_id = data_request['app_id']
+        app_version = data_request['app_version']
+        activity_date = datetime.now()
+        activity_date = activity_date.strftime("%Y-%m-%d %H:%M:%S.%f")
+        device_name = data_request['device_name']
+        device_type = data_request['device_type']
+        device_os = data_request['device_os']
+        latitude = float(data_request['latitude'])
+        longitude = float(data_request['longitude'])
+        location_desc = data_request['location_desc']
+
+        data = [[customer_id, app_id, app_version, activity_date,  device_name,  device_type,  device_os, latitude, longitude, location_desc]]
+        data = pd.DataFrame(data, columns =['customer_id', 'app_id', 'app_version', 'activity_date', 'device_name', 'device_type', 'device_os', 'latitude', 'longitude', 'location_desc'])
+        data = data.assign(result=0, keterangan={})
+
+        for i, row in data.iterrows():
+            data_pred = [[customer_id, activity_date,  device_name,  device_type,  device_os, latitude, longitude]]
             data_pred = pd.DataFrame(data_pred, columns =['customer_id', 'activity_date', 'device_name', 'device_type', 'device_os', 'latitude', 'longitude'])
             data_pred['activity_date'] = pd.to_datetime(data_pred['activity_date'], format='%Y-%m-%d %H:%M:%S.%f').apply(lambda x: x.timestamp())
             data_pred['device_name'] = data_pred['device_name'].apply(lambda x: sum(ord(c) for c in x))
